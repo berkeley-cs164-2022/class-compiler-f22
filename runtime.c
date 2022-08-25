d97e9abfb7ac4890fafc9d6c71af50eb3501042b
schasins <schasins@gmail.com>
2022-08-25 12:46:44 -0700

    Status after first day of class (What is a compiler?)

--- runtime.c
+++ runtime.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-int64_t entry() {
-    return 4000000000000;
-}
+extern int64_t entry();
 
 int main(int argc, char **argv) {
   printf("%" PRIi64, entry());
