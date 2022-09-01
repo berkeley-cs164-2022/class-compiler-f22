3053883430509ffb25861511787ca32795afa46a
Sarah E. Chasins <schasins@gmail.com>
2022-08-31 17:36:02 -0700

    Some snippets for exploring shadowing vs. mutation, statements, and use of tuples.

--- lib/sparecode.ml
+++ lib/sparecode.ml
@@ -1,12 +1,92 @@
-let compile_to_file (program: string): unit =
-     let file = open_out "program.s" in
-     output_string file (compile program);
-     close_out file
+(***********************************)
+(* let bindings don't mutate stuff *)
 
- let compile_and_run (program: string): string =
-     compile_to_file program;
-     ignore (Unix.system "nasm program.s -f macho64 -o program.o");
-     ignore (Unix.system "gcc program.o runtime.o -o program");
-     let inp = Unix.open_process_in "./program" in
-     let r = input_line inp in
-     close_in inp; r 
+let x = 1
+let print_x_first_version () = print_endline ("what is our first x? is it *still* 1? \t" ^  (string_of_int  x))
+
+let x = x + 1
+let print_x_second_version () = print_endline ("what is our second x? \t\t\t" ^ string_of_int  x)
+
+(* 'let () = ...' is just a way of getting the output, making sure it's (), then throwing it away *)
+let () = print_endline "----" 
+
+let () = print_x_second_version ()
+let () = print_x_first_version ()
+
+
+let () = print_endline "*******"
+(*********************************)
+(* why don't they mutate stuff?? *)
+
+
+let show_unrolled () =
+    let x = 1 in
+        let print_x_first_version () = print_endline ("what is our first x? is it *still* 1? \t" ^  (string_of_int  x)) in
+            (* what scope are we in here?  so what's x on the line below? *)
+            let x = x + 1 in 
+                (* what scope are we in here?  so what's x on the line below this? *)
+                let print_x_second_version () = print_endline ("what is our second x? \t\t\t" ^ string_of_int  x) in
+                    let () = print_endline "----" in
+                    
+                        let () = print_x_second_version () in
+                            let () = print_x_first_version () in 
+                                ()
+
+let () = show_unrolled ()
+
+
+let () = print_endline "*******"
+(******************************)
+(* statements *)
+
+
+let statement_example_first_version () =
+    print_endline "hi";
+    print_endline "bye"
+
+let statement_example_second_version () = 
+    let () = print_endline "hi" in
+        print_endline "bye"
+
+
+let () = statement_example_first_version ()
+let () = statement_example_second_version ()
+
+
+
+let () = print_endline "*******"
+(******************************)
+(* another shadowing example *)
+
+let y = 10
+
+let shadowing_test =
+    let y = 2 in 
+        Printf.printf "%i\n" y;
+        (let y = 4 in 
+            Printf.printf "%i\n" y);
+        Printf.printf "%i\n" y;
+;          
+
+Printf.printf "%i\n" y
+
+
+let () = print_endline "*******"
+(******************************)
+(* evaluating to 'multiple values' --- just a tuple *)
+
+
+let plusminus a b =
+  a + b, a - b 
+  
+let plusitem, minusitem = plusminus 10 20 
+let () = print_endline (string_of_int plusitem)
+let () = print_endline (string_of_int minusitem)
+
+let plusminustimes a b =
+  a + b, a - b , a * b
+
+let plusitem, minusitem, timesitem = plusminustimes 10 20
+let () = print_endline (string_of_int plusitem)
+let () = print_endline (string_of_int minusitem)
+let () = print_endline (string_of_int timesitem)
